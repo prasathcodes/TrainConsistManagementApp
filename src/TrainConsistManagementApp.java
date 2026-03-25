1cf06c99b9d1ce91845778dcace6c0b5135243c6
prasathcodes <np9009@srmist.edu.in>
2026-03-25 10:26:28 +0530

    UC9: Group bogies by type using Streams

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -9,29 +9,38 @@ public class TrainConsistManagementApp {
         System.out.println("   Train Consist Management App");
         System.out.println("========================================\n");
 
-        System.out.println("=== UC8 - Filter Passenger Bogies Using Streams ===\n");
+        System.out.println("=== UC9 - Group Bogies by Type ===\n");
 
-        HashMap<String, Integer> bogieCapacity = new HashMap<>();
+        List<Map.Entry<String, Integer>> bogies = new ArrayList<>();
 
-        bogieCapacity.put("Sleeper", 72);
-        bogieCapacity.put("AC Chair", 56);
-        bogieCapacity.put("First Class", 24);
-        bogieCapacity.put("General", 90);
+        bogies.add(new AbstractMap.SimpleEntry<>("Sleeper", 72));
+        bogies.add(new AbstractMap.SimpleEntry<>("AC Chair", 56));
+        bogies.add(new AbstractMap.SimpleEntry<>("First Class", 24));
+        bogies.add(new AbstractMap.SimpleEntry<>("Sleeper", 70));
+        bogies.add(new AbstractMap.SimpleEntry<>("AC Chair", 60));
 
         System.out.println("All Bogies:");
-        for (Map.Entry<String, Integer> entry : bogieCapacity.entrySet()) {
+        for (Map.Entry<String, Integer> entry : bogies) {
             System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
 
-        System.out.println("\nFiltered Bogies (Capacity > 60):");
+        System.out.println("\nGrouped Bogies:\n");
 
-        bogieCapacity.entrySet()
-                .stream()
-                .filter(entry -> entry.getValue() > 60)
-                .forEach(entry ->
-                        System.out.println(entry.getKey() + " -> " + entry.getValue())
-                );
+        Map<String, List<Integer>> grouped =
+                bogies.stream()
+                        .collect(Collectors.groupingBy(
+                                Map.Entry::getKey,
+                                Collectors.mapping(Map.Entry::getValue, Collectors.toList())
+                        ));
 
-        System.out.println("\nUC8 filtering completed...");
+        for (Map.Entry<String, List<Integer>> entry : grouped.entrySet()) {
+            System.out.println("Bogie Type: " + entry.getKey());
+            for (Integer capacity : entry.getValue()) {
+                System.out.println("  Capacity -> " + capacity);
+            }
+            System.out.println();
+        }
+
+        System.out.println("UC9 grouping completed...");
     }
 }
